233f3ff17c95d08506f7254ad8a81f8bbe2a650e
DimaG <dimagilev91@gmail.com>
2024-03-10 20:11:42 +0500

    double jump

--- scenes/elements/characters/ninja_frog/ninja_frog.gd
+++ scenes/elements/characters/ninja_frog/ninja_frog.gd
@@ -5,15 +5,33 @@ extends CharacterBody2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -350.0
+var jumps_count = 0
 
-
-func _physics_process(delta):
+var double_jump_fineshed = true
+func _physics_process(delta):	
+	if is_on_floor():
+		jumps_count = 0
+		
 	if not is_on_floor():
+		if jumps_count == 2:
+			anim.play("DoubleJump")
+			jumps_count += 1
+			double_jump_fineshed = false
+			await anim.animation_finished
+			double_jump_fineshed = true
+		elif double_jump_fineshed:
+			anim.play("Jump")
 		velocity.y += gravity * delta
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		jumps_count += 1
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("ui_accept") and not is_on_floor():
+		if jumps_count < 2:		
+			jumps_count += 1
+			velocity.y = JUMP_VELOCITY
 
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction > 0:
@@ -22,10 +40,12 @@ func _physics_process(delta):
 		anim.flip_h = true
 		
 	if direction:
-		anim.play("Run")
+		if is_on_floor():
+			anim.play("Run")
 		velocity.x = direction * SPEED
 	else:
-		anim.play("Idel")
+		if is_on_floor():
+			anim.play("Idel")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	
